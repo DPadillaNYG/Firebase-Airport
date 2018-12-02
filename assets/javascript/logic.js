046c762f416fd473e630f2545fe321c174292167
DPadillaNYG <DPadilla.NYG@gmail.com>
2018-12-02 06:19:38 -0500

    HTML/CSS

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,9 +1,8 @@
+// This function provides a real-time ticking clock
 function displayTime() {
   var time = moment().format("LTS");
   $("#clock").html(time);
   setTimeout(displayTime, 1000);
 }
 
-$(document).ready(function() {
-  displayTime();
-});
+displayTime();
